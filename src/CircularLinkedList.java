09da4862042c904ef7734b22e770cfab08cd4cbd
ItsEricSun <ericsun828@gmail.com>
2021-10-26 12:37:38 -0400

    2

---
+++ src/CircularLinkedList.java
@@ -0,0 +1,81 @@
+public class CircularLinkedList<E> {
+	private Node<E> head = null; 
+	private Node<E> cur = null;
+	
+	private class Node<E> {
+		E data;
+		Node<E> next;
+		Node(E d) { 
+			data = d; 
+		}
+		
+		Node(E d, Node<E> next){
+			data = d;
+			this.next = next;
+		}
+	}
+	
+	public void add(E data){
+		Node<E> new_node = new Node<E>(data);
+		if(head == null) {
+			head = new_node;
+			new_node.next = head;
+			cur = head;
+		}
+		cur.next = new_node;
+		new_node.next = head;
+		cur = cur.next;
+	}
+	
+	public Node<E> getHead() {
+		return head;
+	}
+	
+	public Node<E> getCur() {
+		return cur;
+	}
+	
+	public void setCur(E data) {
+		cur.data = data;
+	}
+	
+	public void next() {
+		cur = cur.next;
+	}
+
+	public void printList(){
+		if(head == null) {
+			System.out.println("Empty");
+			return;
+		}
+		Node<E> currNode = head;
+		System.out.print("Forwards: ");
+		System.out.print(currNode.data + " ");
+		currNode = currNode.next;
+		while (currNode != head) {
+			System.out.print(currNode.data + " ");
+			currNode = currNode.next;
+		}
+	}
+
+	public int size(){
+		Node<E> currNode = head;
+		int i = 0;
+		while (currNode != null) {
+			currNode = currNode.next;
+			i++;
+		}
+		return i;
+	}
+	
+	public static void main(String[] args) {
+		CircularLinkedList<Player> l = new CircularLinkedList<>();
+		l.add(new Player("A"));
+		l.printList();
+		l.add(new Player("C"));
+		l.add(new Player("B"));
+		l.printList();
+		
+		
+	}
+}
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,98 @@
 
 public class Player {
-	String name;
-	int single;
+	private String name;
+	private int singles = 0;
+	private int doubles = 0;
+	private int triples = 0;
+	private int homeruns = 0;
+	private int walks = 0;
+	private int outs = 0;
 	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSingles() {
+		return singles;
+	}
+
+	public void setSingles(int singles) {
+		this.singles = singles;
+	}
+	
+	public void addSingles() {
+		singles = singles++;
+	}
+
+	public int getDoubles() {
+		return doubles;
+	}
+
+	public void setDoubles(int doubles) {
+		this.doubles = doubles;
+	}
+	
+	public void addDoubles() {
+		doubles = doubles++;
+	}
+
+	public int getTriples() {
+		return triples;
+	}
+
+	public void setTriples(int triples) {
+		this.triples = triples;
+	}
+	
+	public void addTriples() {
+		triples = triples++;
+	}
+
+	public int getHomeruns() {
+		return homeruns;
+	}
+
+	public void setHomeruns(int homeruns) {
+		this.homeruns = homeruns;
+	}
+	
+	public void addHomeruns() {
+		homeruns = homeruns++;
+	}
+
+	public int getWalks() {
+		return walks;
+	}
+
+	public void setWalks(int walks) {
+		this.walks = walks;
+	}
+	
+	public void adWalks() {
+		walks = walks++;
+	}
+
+	public int getOuts() {
+		return outs;
+	}
+
+	public void setOuts(int outs) {
+		this.outs = outs;
+	}
+	
+	public void addOuts() {
+		outs = outs++;
+	}
 }
